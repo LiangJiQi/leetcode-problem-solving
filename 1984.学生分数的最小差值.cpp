e7466ec7febc354421d6efbb03a7ea147f101399
Yunchan <15296361047@163.com>
2022-12-16 18:05:31 +0800

    选择排序和插入排序

--- 1984.学生分数的最小差值.cpp
+++ 1984.学生分数的最小差值.cpp
@@ -56,6 +56,22 @@ public:
         return;
     }
 
+    void BuildHead(vector<int>& nums, int iBeginaIndex, int iEndIndex);
+
+    void HeadSort(vector<int>& nums)
+    {
+        int iLastIndex = nums.size() - 1;
+        for(int i = ( - 1) / 2;i >= 0;i--)
+        {
+            BuildHead(nums, i, nums.size()-1);
+        }
+        while ()
+        {
+            /* code */
+        }
+        
+    }
+
     int minimumDifference(vector<int>& nums, int k) {
         int iResult = __INT_MAX__;
         //常规解法，先排序，后遍历一遍取k区间首尾差值最小
@@ -71,16 +87,43 @@ public:
 
         //堆排序
         
+        
         //https://zhuanlan.zhihu.com/p/42586566
         //选择排序
         //在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
         //从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
         //重复第二步，直到所有元素均排序完毕。
+        // for(int i = 0;i < nums.size();i++)
+        // {
+        //     int iMinIndex = i;
+        //     for(int j = i;j < nums.size();j++)
+        //     {
+        //         if(nums[j] < nums[iMinIndex])
+        //         {
+        //             iMinIndex = j;
+        //         }
+        //     }
+        //     swap(nums[i],nums[iMinIndex]);
+        // }
 
         //插入排序
         //把待排序的数组分成已排序和未排序两部分，初始的时候把第一个元素认为是已排好序的。
         //从第二个元素开始，在已排好序的子数组中寻找到该元素合适的位置并插入该位置。
         //重复上述过程直到最后一个元素被插入有序子数组中。
+        // for(int i = 1;i < nums.size();i++)
+        // {
+        //     int iInsterIndex = i;
+        //     for(int j = 0;j < i;j++)
+        //     {
+        //         if(nums[j] > nums[i])
+        //         {
+        //             iInsterIndex = j;
+        //             break;
+        //         }
+        //     }
+        //     nums.insert(nums.begin() + iInsterIndex,nums[i]);
+        //     nums.erase(nums.begin() + i + 1);
+        // }
 
 
         //取k区间首尾差值最小值
